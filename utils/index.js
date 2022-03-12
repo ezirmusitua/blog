db0de1503e487b297dea058227c26b28ec942f15
ezirmusitua <jferroal@gmail.com>
2022-03-12 13:00:08 +0800

    Feat: sort post by date or title

--- utils/index.js
+++ utils/index.js
@@ -1,3 +1,12 @@
 export const sortByDate = (a, b) => {
-  return new Date(b.frontmatter.date) - new Date(a.frontmatter.date)
+  const prev = new Date(a.frontmatter.date);
+  const next = new Date(b.frontmatter.date);
+  const invalid_prev = prev.toString() === "Invalid Date";
+  const invalid_next = next.toString() === "Invalid Date";
+  if (invalid_prev && invalid_next) {
+    return a.frontmatter.title.localeCompare(b.frontmatter.title)
+  }
+  if (invalid_prev) return 1;
+  if (invalid_next) return -1;
+  return next - prev;
 }
