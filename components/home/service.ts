72471c47079c4203a58803dca0b3c5506934e6f2
ezirmusitua <jferroal@gmail.com>
2023-07-07 09:49:48 +0800

    Feat: remove unused

--- components/home/service.ts
+++
@@ -1,8 +0,0 @@
-import { api_config } from "config";
-import { iFrontMatter } from "schema/post";
-
-export async function list_post(): Promise<({ id: string } & iFrontMatter)[]> {
-  const resp = await fetch(api_config.resource("/api/post"), { method: "GET" });
-  const { posts } = await resp.json();
-  return posts;
-}
