1ea391381cdc9b2e5177ad360214483b4d8969bd
ezirmusitua <jferroal@gmail.com>
2023-09-20 23:46:43 +0800

    Fix: build standalone with cards

--- .scripts/build_standalone.sh
+++ .scripts/build_standalone.sh
@@ -2,6 +2,7 @@
 next build
 cp -r public .next/standalone/public
 cp -r posts .next/standalone/posts
+cp -r cards .next/standalone/cards
 cp -r .next/static .next/standalone/.next/static
 cp -r .artifacts .next/standalone/.artifacts
 cp .env.local .next/standalone/.env.local
